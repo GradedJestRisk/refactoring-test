bbbf6dfcfa31b74866800733be7f63427eed64a3
GradedJestRisk <graded.jest.risk@gmail.com>
2020-05-17 15:07:53 +0200

    [OOP - Hexagonal] Modify

--- src/object-oriented/hexagonal-event/code/domain/EmailChangedEvent.js
+++ src/object-oriented/hexagonal-event/code/domain/EmailChangedEvent.js
@@ -3,10 +3,6 @@ class EmailChangedEvent {
         this.userId = userId;
         this.newEmail = newEmail;
     }
-
-    equals(other) {
-        return (this.userId === other.userId && this.newEmail === other.newEmail);
-    }
 }
 
 module.exports = EmailChangedEvent;
