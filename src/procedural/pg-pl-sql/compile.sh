f0a4a5295dec4d5680e5635188a59b924f78cbae
GradedJestRisk <graded.jest.risk@gmail.com>
2020-06-15 09:15:37 +0200

    [Procedural DB] Add missing compile

--- src/procedural/pg-pl-sql/compile.sh
+++ src/procedural/pg-pl-sql/compile.sh
@@ -5,6 +5,12 @@ psql -U postgres -d refactoring_test -h localhost -p 8432 <<HEREDOC
 BEGIN;
 
 \i user-type-from-email.sql
+\i email-change-authorized.sql
+\i get-execution-time.sql
+\i propagate-change.sql
+\i update-company.sql
+\i update-user.sql
+\i user-type-from-email.sql
 
 \i change-user-email.sql
 
