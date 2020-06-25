1942efb5848a77a361bc870fc76c1034e4fbfe0a
GradedJestRisk <graded.jest.risk@gmail.com>
2020-06-25 13:40:29 +0200

    [OOP - Hexagonal] Extract exports to body(refacto)

--- src/object-oriented/hexagonal-event/code/user-side/user-handler.js
+++ src/object-oriented/hexagonal-event/code/user-side/user-handler.js
@@ -7,20 +7,20 @@ const message = {
     USER_DOES_NOT_EXISTS: 'user does not exists'
 }
 
-module.exports = {
-    async getUser(request, h) {
+const getUser = async function (request, h) {
 
-        const id = parseInt(request.params.id);
-        const data = await controller.getUser(id);
+    const id = parseInt(request.params.id);
+    const data = await controller.getUser(id);
 
-        if (data ===  message.USER_DOES_NOT_EXISTS) {
-            throw Boom.notFound("User not found: " + id);
-        }
-
-        return h.response({
-            user: data
-        })
-            .code(200)
-            .header('Content-Type', 'application/json;charset=UTF-8')
+    if (data ===  message.USER_DOES_NOT_EXISTS) {
+        throw Boom.notFound("User not found: " + id);
     }
-};
+
+    return h.response({
+        user: data
+    })
+        .code(200)
+        .header('Content-Type', 'application/json;charset=UTF-8')
+}
+
+module.exports = { getUser };
